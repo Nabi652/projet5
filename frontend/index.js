3bf37fce1681cdafa69b08bac9b2345ce503b829
nabila hajou <nab.ikk13@gmail.com>
2021-08-16 19:26:33 +0200

    mise en commentaire de <main> de index.html et import des images et infos de chaque ourson sur js via localhost

--- frontend/index.js
+++ frontend/index.js
@@ -1,53 +1,64 @@
-// let tableau = [
-//   {
-//     name: "norbert",
-//     price: "2900",
-//     imageUrl: "https://dummyimage.com/100x100/ccc/555",
-//   },
+let tableau = [
+  {
+    name: "norbert",
+    price: "2900",
+    imageUrl: "https://dummyimage.com/100x100/ccc/555",
+  },
+];
 
-//   {
-//     name: "nom2",
-//     price: "5000",
-//     imageUrl: "https://dummyimage.com/100x100/ccc/555",
-//   },
-// ];
+function afficheList(tableau) {
+  for (let ours of tableau) {
+    // creation d'une div : <div></div>
+    let div = document.createElement("div");
 
-// function afficheList(tableau) {
-//   for (let ours of tableau) {
-//     // console.log(ours.name);
-//     // creation d'une div : <div></div>
-//     let div = document.createElement("div");
+    // <div class="cell"></div>
+    div.setAttribute("class", "cell");
 
-//     // <div class="cell"></div>
-//     div.setAttribute("class", "cell");
+    //creation h2
+    let h2 = document.createElement("h2");
 
-//     //creation h2
-//     let h2 = document.createElement("h2");
+    //contenu h2 :<h2>Norbert</h2>
+    h2.textContent = ours.name;
 
-//     //contenu h2 :<h2>Norbert</h2>
-//     h2.textContent = ours.name;
+    //mettre la balise h2 dans la div
+    div.appendChild(h2);
 
-//     //mettre la balise h2 dans la div
-//     div.appendChild(h2);
+    //creation d'une balise image
+    let image = document.createElement("img");
+    image.setAttribute("src", ours.imageUrl);
+    image.setAttribute("width", "100");
 
-//     //creation d'une balise image
-//     let image = document.createElement("img");
-//     image.setAttribute("src", ours.imageUrl);
-//     image.setAttribute("width", "100");
+    //ajout de l'image dans la div
+    div.appendChild(image);
 
-//     //ajout de l'image dans la div
-//     div.appendChild(image);
+    //mettre la div cell dans le container
+    document.getElementById("list").appendChild(div);
 
-//     //mettre la div cell dans le container
-//     document.getElementById("list").appendChild(div);
-//   }
-// }
+    //creation d'une balise <p> pour description
+    let description = document.createElement("p");
 
-// let url = "http://localhost:3000/api/teddies";
+    // contenu du p (descriptinon)
+    description.textContent = ours.description;
 
-// fetch(url)
-//   .then((response) => response.json())
-//   .then((tableau) => {
-//     console.log(tableau);
-//     afficheList(tableau);
-//   });
+    // ajout du <p> à la div
+    div.appendChild(description);
+
+    //creation du <h3> pour le prix
+    let prix = document.createElement("h3");
+
+    //contenu du <h3> (prix)
+    prix.textContent = ours.price;
+
+    //ajout du <h3> à la div cell
+    div.appendChild(prix);
+  }
+}
+
+let url = "http://localhost:3000/api/teddies";
+
+fetch(url)
+  .then((response) => response.json())
+  .then((tableau) => {
+    console.log(tableau);
+    afficheList(tableau);
+  });
