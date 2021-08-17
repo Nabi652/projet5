ba711a78df6729a13da4a103cb268e29909deae3
nabila hajou <nab.ikk13@gmail.com>
2021-08-17 16:02:36 +0200

    ajout du a href sur les images qui mène à la page produit et deplacement de certaines variables pour que soient d'abord affichés le nom, la photo, la description et le prix

--- frontend/index.js
+++ frontend/index.js
@@ -12,7 +12,10 @@ function afficheList(tableau) {
     let div = document.createElement("div");
 
     // <div class="cell"></div>
-    div.setAttribute("class", "cell");
+    div.setAttribute(
+      "class",
+      "cell box-norbert box-arnold box-lenny box-gustav box-garfunkel"
+    );
 
     //creation h2
     let h2 = document.createElement("h2");
@@ -26,7 +29,6 @@ function afficheList(tableau) {
     //creation d'une balise image
     let image = document.createElement("img");
     image.setAttribute("src", ours.imageUrl);
-    image.setAttribute("width", "100");
 
     //ajout de l'image dans la div
     div.appendChild(image);
@@ -34,10 +36,36 @@ function afficheList(tableau) {
     //mettre la div cell dans le container
     document.getElementById("list").appendChild(div);
 
+    // creation de la variable boxImage
+    let boxImage = document.createElement("div");
+
+    // ajout de boxImage à la div cell
+    div.appendChild(boxImage);
+
+    //ajout de l'image à la div boxImage
+    boxImage.appendChild(image);
+
+    // j'ai donné une class à la variable boxImage => "box.img"
+    boxImage.setAttribute("class", "box-img");
+
+    // creation lien sur les photos
+    //creation d'une balise <a>
+    let lien = document.createElement("a");
+
+    //ajout du <a> à la div cell
+    div.appendChild(lien);
+
+    // ajout d'un hrf avec le lien de la page produit
+    lien.setAttribute("href", "./produit.html");
+
+    //ajout de l'image au a
+    lien.appendChild(image);
+    lien = "./produit.html";
+
     //creation d'une balise <p> pour description
     let description = document.createElement("p");
 
-    // contenu du p (descriptinon)
+    // contenu du p (description)
     description.textContent = ours.description;
 
     // ajout du <p> à la div
