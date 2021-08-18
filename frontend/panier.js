5e887b9c9112bcc1d054c818534969427f73a571
nabila hajou <nab.ikk13@gmail.com>
2021-08-18 23:58:22 +0200

    ajout des balise ul et li pour le panier de la page panier via js

---
+++ frontend/panier.js
@@ -0,0 +1,79 @@
+//création d'une balise <main>
+let main = document.querySelector("main");
+
+//creation div class= box-prix
+let boxPrix = document.createElement("div");
+
+//ajout class = box-prix à la div (variable)
+boxPrix.setAttribute("class", "box-prix");
+
+//ajout de la div à <main>
+main.appendChild(boxPrix);
+
+//creation balise <ul>
+let ligne1 = document.createElement("ul");
+
+//ajouter ul à box-prix (variable boxPrix)
+boxPrix.appendChild(ligne1);
+
+//creation du premier <li>
+let li1 = document.createElement("li");
+
+//ajout de contenu à li1
+li1.textContent = "Produit";
+
+//ajout de <li> à <ul>
+ligne1.appendChild(li1);
+
+//creation du deuxième <li>
+let li2 = document.createElement("li");
+
+//ajout de contenu à li1
+li2.textContent = "Quantité";
+
+//ajout de <li> à <ul>
+ligne1.appendChild(li2);
+
+//creation du troisième <li>
+let li3 = document.createElement("li");
+
+//ajout de contenu à li1
+li3.textContent = "Prix";
+
+//ajout de <li> à <ul>
+ligne1.appendChild(li3);
+
+//creation du quatrième <li>
+let li4 = document.createElement("li");
+
+//ajout de contenu à li1
+li4.textContent = "Total";
+
+//ajout de <li> à <ul>
+ligne1.appendChild(li4);
+
+//----------------- creation de <ul>
+
+//creation balise <ul>
+let ligne2 = document.createElement("ul");
+
+//ajouter ul à box-prix (variable boxPrix)
+boxPrix.appendChild(ligne2);
+
+//creation du cinquieme <li>
+let li5 = document.createElement("li");
+
+//ajout de contenu à li1
+li5.textContent = "Total";
+
+//ajout de <li> à <ul>
+ligne2.appendChild(li5);
+
+//creation du sixième <li>
+let li6 = document.createElement("li");
+
+//ajout de contenu à li1
+li6.textContent = "Quantité";
+
+//ajout de <li> à <ul>
+ligne2.appendChild(li6);
